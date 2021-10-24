a7754c538076d67e5277cd492b71fc19dc5cd972
Eric Lamontagne <ricklamontagne@gmail.com>
2021-10-24 12:44:44 -0400

    feat: Leverage The Graph to fetch the price (value in dollars) of cryptocurrency from  Uniswap

--- src/components/App.js
+++ src/components/App.js
@@ -9,47 +9,48 @@ import uniswapLogo from '../uniswap-logo.png'
 import daiLogo from '../dai-logo.png'
 
 export const client = new ApolloClient({
-  link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2'
-  }),
-  fetchOptions: {
-    mode: 'no-cors'
-  },
-  cache: new InMemoryCache()
-})
+	link: new HttpLink({
+		uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2'
+	}),
+	fetchOptions: {
+		mode: 'no-cors'
+	},
+	cache: new InMemoryCache()
+}) 
 
 const DAI_QUERY = gql`
-  query tokens($tokenAddress: Bytes!) {
-    tokens(where: { id: $tokenAddress }) {
-      derivedETH
-      totalLiquidity
-    }
-  }
+	query tokens($tokenAddress: Bytes!) {
+		tokens(where: { id: $tokenAddress }) {
+			derivedETH
+			totalLiquidity
+		}
+	}
 `
 
 const ETH_PRICE_QUERY = gql`
-  query bundles {
-    bundles(where: { id: "1" }) {
-      ethPrice
-    }
-  }
+	query bundles {
+		bundles(where: { id: "1" }) {
+			ethPrice
+		}
+	}
 `
 
 function App() {
-  const { loading: ethLoading, data: ethPriceData } = useQuery(ETH_PRICE_QUERY)
+	
+	const { loading: ethLoading, data: ethPriceData } = useQuery(ETH_PRICE_QUERY)
   const { loading: daiLoading, data: daiData } = useQuery(DAI_QUERY, {
     variables: {
       tokenAddress: '0x6b175474e89094c44da98b954eedeac495271d0f'
     }
   })
 
-  const daiPriceInEth = daiData && daiData.tokens[0].derivedETH
+	const daiPriceInEth = daiData && daiData.tokens[0].derivedETH
   const daiTotalLiquidity = daiData && daiData.tokens[0].totalLiquidity
   const ethPriceInUSD = ethPriceData && ethPriceData.bundles[0].ethPrice
 
   return (
     <div>
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+    <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
           href="http://www.dappuniversity.com/bootcamp"
@@ -85,7 +86,7 @@ function App() {
             </div>
           </main>
         </div>
-      </div>
+      </div> 
     </div>
   );
 }
